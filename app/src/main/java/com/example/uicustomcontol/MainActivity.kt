6a7756c8b5fff2435bd628e54051fb34842ea596
小柯 <3324245065@qq.com>
2023-04-12 09:17:38 +0800

    修复了因旋转或各种意外导致的数据丢失的问题

--- app/src/main/java/com/example/uicustomcontol/MainActivity.kt
+++ app/src/main/java/com/example/uicustomcontol/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uicustomcontol
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,18 +8,30 @@ import com.example.uicustomcontol.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val num = 0
+    private var num = 0
+    private var KEY_num = "KEY_num"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        num = getSharedPreferences("data", Context.MODE_PRIVATE).getInt(KEY_num, 0)
+        binding.textnum.text = num.toString()
+
         binding.title.setTitle("首页")
         binding.title.setEdit {
             Toast.makeText(this, "点击了编辑", Toast.LENGTH_LONG).show()
         }
 
         binding.btnAdd.setOnClickListener {
+            num++
             binding.textnum.text = num.toString()
+            Save()
         }
     }
+
+    private fun Save() =
+        getSharedPreferences("data", Context.MODE_PRIVATE).edit().apply { putInt(KEY_num, num) }
+            .apply()
+
 }
